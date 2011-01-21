9f3f897a0e1cae6c6f7f16a0da70fe71a538b66b
Kim Eunsoo <hahah@kaist.ac.kr>
2011-01-21 16:25:27 +0900

    use one file....:(

--- kaistsms.rb
+++ kaistsms.rb
@@ -1,13 +1,25 @@
 require 'rubygems'
 require 'mechanize'
-require 'kaistmail.rb'
 
-m = KaistMail.new
-m.login
-if m.login?
-		  puts "Login Success"
-else
-		  puts "Login Fail"
+class KaistMail
+	def login userid, userpasswd
+		m = Mechanize.new
+		mail = m.get 'http://mail.kaist.ac.kr'
+		main = mail.frame('main').click
+		@login = main.form('login') do |f|
+			f.USERS_ID = userid
+			f.USERS_PASSWD = userpasswd
+			f.action = 'https://mail.kaist.ac.kr/nara/servlet/user.UserServ'
+			f.cmd = 'login'
+		end.submit
+	end
+
+	def login?
+		if @login.body=~/img_msgError.gif/
+				false
+		else
+				true
+		end
+	end
 end
-
 
