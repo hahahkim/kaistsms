963e1274cdd51b6168fbedf1afc2c4c74198e686
Kim Eunsoo <hahah@kaist.ac.kr>
2011-01-21 16:16:05 +0900

    kaistmail updated

--- kaistmail.rb
+++ kaistmail.rb
@@ -2,29 +2,23 @@ require 'rubygems'
 require 'mechanize'
 
 class KaistMail
-	def initialize
-	end
-	
-	def login
+	def login userid, userpasswd
 		m = Mechanize.new
 		mail = m.get 'http://mail.kaist.ac.kr'
 		main = mail.frame('main').click
-		print "input id : "
-		userid = gets.chomp
-		print "input passwd : "
-		userpasswd = gets.chomp
 		@login = main.form('login') do |f|
 			f.USERS_ID = userid
 			f.USERS_PASSWD = userpasswd
 			f.action = 'https://mail.kaist.ac.kr/nara/servlet/user.UserServ'
 			f.cmd = 'login'
 		end.submit
-		if login.body=~/img_msgError.gif/
-			puts "Login Error"
-			nil
+	end
+
+	def login?
+		if @login.body=~/img_msgError.gif/
+				false
 		else
-			puts "Login Success"
-			@login
+				true
 		end
 	end
 end
